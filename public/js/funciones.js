857fb08c2584cead8ce6aeb27d870078c15bc838
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2023-01-20 23:09:01 +0100

    logica basica de tiradas

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,3 +1,48 @@
 $(document).ready(function() {
 
+	//definir como const?
+	let puntFinalJugadores = [];
+	let numJugadores = 1;
+	let numCarasDado = 6;
+	let numDados = 5;
+	let numTiradas = 3;
+
+	//inicializamos puntuaciones de jugadores
+	for (let i = 1; i <= numJugadores; i++) {
+		puntFinalJugadores[i] = 0;
+	}
+
+	for (let turno = 1; turno <= numCarasDado; turno++) {
+		//posibilidad de interrumpir juego?
+		console.log("Turno: " + turno);
+		for (let jugador = 1; jugador <= numJugadores; jugador++) {
+			//posibilidad de comparar puntuaciones de jugadores
+			console.log("\tJugador: " + jugador);
+			for (let tirada = 1; tirada <= numTiradas; tirada++) {
+				console.log("\t\tTirada: " + tirada);
+				for (let dado = 1; dado <= numDados; dado++) {
+					let num = Math.ceil(Math.random() * numCarasDado);
+					console.log("\t\t\tDado " + dado + ": " + num);
+				}
+			}
+			//console.log("Puntuación de Jugador " + jugador + ": " + puntFinalJugadores[jugador]);
+		}
+	}
+
+/*
+	Repetimos 5 veces (1 por dado)
+		Turno 1 
+			Jugador 1
+				Repetimos 3 veces (como máximo)
+					Lanzamiento 1 
+						Tiramos 5 dados
+						Vemos el resultado/puntos
+						Escogemos Dados para guardar
+					Lanzamiento X 
+						...
+			Jugador X
+				...
+		Turno 2
+			...
+*/
 });
