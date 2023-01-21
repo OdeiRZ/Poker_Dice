3df854ab9709c3d796f2ca62ef806b05dc327708
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2023-01-21 01:03:46 +0100

    creamos checkboxs dinamicos

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -17,24 +17,30 @@ $(document).ready(function() {
 	$("#tablero").append("Comienzo de Partida<br>");
 	for (let turno = 1; turno <= numCarasDado; turno++) {
 		//posibilidad de interrumpir juego?
-		$("#tablero").append("&nbsp; Turno " + turno + "<br>");
+		$("#tablero").append("&nbsp; &nbsp; Turno " + turno + "<br>");
 		for (let jugador = 1; jugador <= numJugadores; jugador++) {
 			//posibilidad de comparar puntuaciones de jugadores
-			$("#tablero").append("&nbsp; &nbsp; Jugador " + jugador + "<br>");
+			$("#tablero").append("&nbsp; &nbsp; &nbsp; &nbsp; Jugador " + jugador + "<br>");
 			for (let tirada = 1; tirada <= numTiradas; tirada++) {
-				$("#tablero").append("&nbsp; &nbsp; &nbsp; Tirada " + tirada + "<br>");
+				$("#tablero").append("&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Tirada " + tirada + "<br>");
 				let tiradas = "";
+				$("#tablero").append("&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ");
 				for (let dado = 1; dado <= numDados; dado++) {
 					//damos por hecho que jugamos con figuras de poker
 					let num = Math.ceil(Math.random() * numCarasDado);
 					if (swPoker) {
 						num = figurasPoker[num];
 					}
-					tiradas += num + " ";
+					//tiradas += num + " ";
+					$("#tablero").append('<input type="checkbox" id="'+dado+'_'+tirada+'_'+jugador+'_'+turno+'" name="'+dado+'_'+tirada+'_'+jugador+'_'+turno+'" value="'+num+'">');
+					$("#tablero").append('<label for="'+dado+'_'+tirada+'_'+jugador+'_'+turno+'">'+num+'</label>');
+					//<input type='checkbox' id='dado_'+dado name='vehicle1' value='Bike'>
+  					//<label for="vehicle1"> I have a bike</label>
 				}
-				$("#tablero").append("&nbsp; &nbsp; &nbsp; &nbsp; " + tiradas + "<br>");
+				$("#tablero").append("<br>");
+				//$("#tablero").append("&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; " + tiradas + "<br>");
 			}
-			$("#tablero").append("&nbsp; Puntuación de Jugador " + jugador + ": " + puntFinalJugadores[jugador] + "<br>");
+			$("#tablero").append("&nbsp; &nbsp; Puntuación de Jugador " + jugador + ": " + puntFinalJugadores[jugador] + "<br>");
 		}
 	}
 
