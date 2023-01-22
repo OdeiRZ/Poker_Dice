0f00d2e301951996bae8ad2986286e879e4498b2
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2023-01-22 17:05:37 +0100

    agregamos funcionalidad a botones

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,13 +1,22 @@
-$(document).ready(function() {
 
-	//definir como const?
-	let puntFinalJugadores = [];
-	let numJugadores = 1;
-	let numCarasDado = 6;
-	let swPoker = true;
-	let figurasPoker = ["", "7", "8", "J", "Q", "K", "•"];
-	let numDados = 5;
-	let numTiradas = 3;
+var numJugadores;
+var numCarasDado;
+var numDados;
+var numTiradas;
+var swPoker;
+var puntFinalJugadores = [];
+const figurasPoker = ["", "7", "8", "J", "Q", "K", "•"];
+
+$(document).ready(function() {
+	//https://programadorwebvalencia.com/sencillo-boton-on-off-html-y-css/
+/*
+	var numJugadores = 1;
+	var numCarasDado = 6;
+	var numDados = 5;
+	var numTiradas = 3;
+	var swPoker = true;
+	var puntFinalJugadores = [];
+	const figurasPoker = ["", "7", "8", "J", "Q", "K", "•"];
 
 	//inicializamos puntuaciones de jugadores
 	for (let i = 1; i <= numJugadores; i++) {
@@ -34,8 +43,6 @@ $(document).ready(function() {
 					//tiradas += num + " ";
 					$("#tablero").append('<input type="checkbox" id="'+dado+'_'+tirada+'_'+jugador+'_'+turno+'" name="'+dado+'_'+tirada+'_'+jugador+'_'+turno+'" value="'+num+'">');
 					$("#tablero").append('<label for="'+dado+'_'+tirada+'_'+jugador+'_'+turno+'">'+num+'</label>');
-					//<input type='checkbox' id='dado_'+dado name='vehicle1' value='Bike'>
-  					//<label for="vehicle1"> I have a bike</label>
 				}
 				$("#tablero").append("<br>");
 				//$("#tablero").append("&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; " + tiradas + "<br>");
@@ -43,7 +50,7 @@ $(document).ready(function() {
 			$("#tablero").append("&nbsp; &nbsp; Puntuación de Jugador " + jugador + ": " + puntFinalJugadores[jugador] + "<br>");
 		}
 	}
-
+*/
 /*
 	Repetimos 5 veces (1 por dado)
 		Turno 1 
@@ -61,3 +68,77 @@ $(document).ready(function() {
 			...
 */
 });
+
+function comenzarJuego() {
+	console.log("_______________________");
+	console.log("Inicializamos variables");
+	console.log("_______________________");
+	inicializarVariables();
+	console.log("Jugadores: " + numJugadores);
+	console.log("Dados: " + numDados);
+	console.log("Caras de Dados: " + numCarasDado);
+	console.log("Tiradas: " + numTiradas);
+	console.log("Dados de Poker: " + ((swPoker) ? "Si" : "No"));
+
+	console.log("__________________________");
+	console.log("Inicializamos Puntuaciones");
+	console.log("__________________________");
+	inicializarPuntuaciones();
+
+	console.log("_________________");
+	console.log("Ocultamos Botones");
+	console.log("_________________");
+	ocultarBotones();
+
+	console.log("________________");
+	console.log("Comenzamos juego");
+	console.log("________________");
+}
+
+function inicializarVariables() {
+	numJugadores = $("#numJugadores").find(":selected").val();
+	numDados = $("#numDados").find(":selected").val();
+	numCarasDado = $("#numCarasDado").find(":selected").val();
+	numTiradas = $("#numTiradas").find(":selected").val();
+	swPoker = $("#swPoker").is(":checked");
+}
+
+function inicializarPuntuaciones() {
+	for (let i = 1; i <= numJugadores; i++) {
+		puntFinalJugadores[i] = 0;
+		console.log("Puntuación de Jugador " + i + ": " + puntFinalJugadores[i]);
+	}
+}
+
+function ocultarBotones() {
+	console.log("Botones Ocultados");
+	$("#panelJugadores").hide();
+	$("#panelDados").hide();
+	$("#panelCarasDado").hide();
+	$("#panelTiradas").hide();
+	$("#panelSwPoker").hide();
+	$("#panelBtnInicio").hide();
+	$("#panelBtnFin").show();
+}
+
+function finalizarJuego() {
+	console.log("_______________");
+	console.log("Finalizar Juego");
+	console.log("_______________");
+
+	console.log("_________________");
+	console.log("Reiniciar Botones");
+	console.log("_________________");
+	reiniciarBotones();
+}
+
+function reiniciarBotones() {
+	console.log("Botones Reiniciados");
+	$("#panelBtnFin").hide();
+	$("#panelJugadores").show();
+	$("#panelDados").show();
+	$("#panelCarasDado").show();
+	$("#panelTiradas").show();
+	$("#panelSwPoker").show();
+	$("#panelBtnInicio").show();
+}
